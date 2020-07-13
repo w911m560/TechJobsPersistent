8affd8081ea14029238d02cf912647105b2d76cd
William Morrison <williamchrismorrison@gmail.com>
2020-07-13 15:50:12 -0700

    final

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -3,9 +3,12 @@
 	--'Name' longtext; 
 	--'EmployerId' int
 --Part 2
-	--SELECT *
-	--FROM employers
-	--WHERE location="St. Louis City"
+	SELECT *
+	FROM employers
+	WHERE location="St. Louis City"
 --Part 3
-	--
+	SELECT Name, Description
+	FROM jobskills 
+	LEFT JOIN skills ON jobskills.SkillId = skills.Id 
+	ORDER BY Name
 
